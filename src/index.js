c07ca42b0205637ca82b60a5360454f5119fb23f
Gustavo Ferreira <gustavodelferreira.12@gmail.com>
2022-03-03 18:05:37 -0300

    meme generator update

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+export default function index() {
+    return (
+        <>
+            <App />
+        </>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
